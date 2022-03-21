748933bc08314b0d2c43180f9d1460c2902b8743
mores <morteza78rezaeian@gmail.com>
2022-03-21 14:22:32 +0330

    123

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
 
 function App() {
+  const boxes = [1, 2, 3, 4];
+
   return (
     <div className='App'>
-     hello world
+      <div className='container'>
+        <div className='ul'>
+          {boxes.map((box) => (
+            <div key={box} className={`box box-${box}`}>
+              {box}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className='box-btn'>
+        <button>+</button>
+        <button>-</button>
+      </div>
     </div>
   );
 }
